810196c0286d8a1c3c9ea80ce8706e29a949980a
Alberto González Martínez <albergonmar@gmail.com>
2020-01-06 00:39:31 +0100

    Añadida la clase de prueba ClienteGraph

---
+++ pruebasServer/src/server/ClienteGraph.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.concurrent.Callable;
+
+/**
+ *
+ * @author Alberto
+ */
+public class ClienteGraph implements Callable<String> {
+
+    Socket cliente;
+
+    DataInputStream entrada;
+    DataOutputStream salida;
+
+    String mensaje, respuesta;
+
+    private String idConexion;
+
+    public ClienteGraph(String id){
+        this.idConexion = id;
+        this.respuesta = "NULL";
+    }
+
+    @Override
+    public String call(){
+
+        try{
+
+            cliente = new Socket("weatherubicuastation.duckdns.org", 8080);
+
+            entrada = new DataInputStream(cliente.getInputStream());
+            salida = new DataOutputStream(cliente.getOutputStream());
+
+            mensaje = "Graph-"+"4-"+"temperatura";
+
+            salida.writeUTF(mensaje);
+
+            respuesta = entrada.readUTF();
+            System.out.println(respuesta);
+
+            entrada.close();
+            salida.close();
+            cliente.close();
+
+        }catch(IOException e){
+
+            System.out.println("Error: "+e.getMessage());
+        }
+
+        return respuesta;
+    }
+}
